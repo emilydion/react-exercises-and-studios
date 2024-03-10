f7c5c863dd0f69663e3f202162f02c7f77771e26
emilydion <edion468@gmail.com>
2024-03-10 12:00:35 -0500

    added updated styling and current components

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
+import classes from './BookList.module.css';
+
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Read This Month";
+   let book1 = "https://upload.wikimedia.org/wikipedia/commons/1/14/Misery_%281987%29_front_cover%2C_first_edition.jpg";
+   let book2 = "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1622355533i/4667024.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/91wKDODkgWL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Misery by Steven King" />
+         <img src={book2} alt="The Help By Kathryn Stockett" />
+         <img src={book3} alt="Harry Potter and the Sorcerer's Stone by J.K. Rowling" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+      <>
+         <h2 className={classes.choresHeading}>Chores</h2>
+         <p className={classes.choresText}>No content is here!</p>
+      </>
+   );
 }
